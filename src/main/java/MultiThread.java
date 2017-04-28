989661b8d273f8d047e292d410e055a49fd887b4
DefNeoRoman <d89056411060@yandex.ru>
2017-04-28 07:54:16 +0300

    новое решение

--- src/main/java/MultiThread.java
+++ src/main/java/MultiThread.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.*;
 
 /**
  * Created by роман on 27.04.2017.
@@ -10,21 +11,36 @@ import java.nio.file.Path;
 public class MultiThread {
     public static void main(String[] args) throws IOException {
         long start = System.currentTimeMillis();
-
-       walk("D:\\forJava");
+        walk("D:\\forJava\\examples");
         long finish = System.currentTimeMillis();
         long timeConsumedMillis = finish - start;
-        System.out.println("Программа выполнена за: " + timeConsumedMillis);
+        System.out.println("Время выполнения:" + timeConsumedMillis);
+
     }
     public static void walk(String pathName) throws IOException{
-
+        List<Entity> myfiles =
+                new ArrayList<>();
         Path startPath = FileSystems.getDefault().getPath(pathName);
-        Files.walk(startPath)
-                .filter( path -> path.toFile().isFile())
-                .filter( path -> path.toString().endsWith(".docx"))
-                .forEach( System.out::println );
+        Files.walk(startPath).parallel().filter(f->{
+            if ("executors".equals(f.getFileName().toString())
+                    || "kalugaTest".equals(f.getFileName().toString())){
+
+                return true;
+            }
+            return false;
+        }).forEach(f -> {
+            myfiles.add(
+                    new Entity(
+                            f.getFileName().toString(),
+                            new Date(f.getFileName().toFile().lastModified()),
+                            f.getFileName().toFile().length()));
+        });
+
+        myfiles.stream().sorted(new EntityComparator()).forEach(System.out::println);
+        System.out.println("найдено файлов: " + myfiles.size());
 
     }
+
 
 }
 
